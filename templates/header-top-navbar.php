dbf6f81306587e5536e00a16cb18e4d3c50f1c5c
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-01-06 14:47:44 -0600

    Navbar edits

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -2,10 +2,7 @@
   <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
+        <span><h2>Navigation</h2></span>
       </button>
       <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
     </div>
