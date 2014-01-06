2b0a3a5eccf177da7788a844fc748cec38de70d4
michael <michael@12southmusic.com>
2014-01-06 15:27:49 -0600

    Last github debug

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,7 +48,7 @@ var UTIL = {
 };
 
 $(document).ready(UTIL.loadEvents);
-//change to test git hub again
+//change to test git hub one last time
 // This fires Isotope
     $(function isotope(){
       //sets container
