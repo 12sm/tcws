77c86ccd6a36116f9b92968b71ac465b52a25dcb
michael <michael@12southmusic.com>
2014-01-06 15:55:49 -0600

    Git Yay!

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-//Git Test Last
+//Git YAY!
 // Modified http://paulirish.com/2009/markup-based-unobtrusive-comprehensive-dom-ready-execution/
 // Only fires on body class (working off strictly WordPress body_class)
 
