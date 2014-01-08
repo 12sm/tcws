3920fd073a023176aadb8a664050d67dfb146ed3
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-01-08 12:21:13 -0600

    Header Banner

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,5 +1,4 @@
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
-  <img src="./assets/img/top-nav-logo.jpg" id="topBanner"/>
   <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
