c86a938978f4410fe151c457516415e2038ddf02
michael <michael@12southmusic.com>
2014-01-08 17:33:16 -0600

    New JS Files

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,23 @@ var ExampleSite = {
   // Home page
   home: {
     init: function() {
-      // JS here
+    
+    	//Scrollto
+	    $(function() {
+		    $('a[href*=#]:not([href=#])').click(function() {
+			    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				    var target = $(this.hash);
+				    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				    if (target.length) {
+					    $('html,body').animate({
+						    scrollTop: target.offset().top
+						 }, 1000);
+						 return false;
+					}
+				}
+		    });
+	   });
+    
       $(window).load(function(){
 
         $( ".width2" ).each(function( i ) {
