6bb2d52abf9d4f50acb66d26c779396b15dd116c
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-01-08 14:29:04 -0600

    Navbar Mobile

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -2,7 +2,7 @@
   <img src="./assets/img/top-nav-logo.jpg" id="topBanner"/>
   <div class="container" id="navbar-container">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+      <button type="button" class="navbar-toggle btn btn-link active" data-toggle="collapse" data-target=".navbar-collapse">
         <span id="navButton"><h1>Navigation</h1></span>
       </button>
       <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
