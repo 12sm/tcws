fb3ae595bb73d74286d3d748df1a2793e38a6b92
michael <michael@12southmusic.com>
2014-01-09 17:30:50 -0600

    Footer images and updates

--- templates/footer.php
+++ templates/footer.php
@@ -1,22 +1,22 @@
 <footer class="content-info container" role="contentinfo">
+<div class="row">
+	<div class="col-md-12">
+		<?php dynamic_sidebar('sidebar-footer'); ?>
+	</div>
+</div> 
   <div class="row">
-    <div class="col-lg-12">
-      <?php dynamic_sidebar('sidebar-footer'); ?>
-      <div id="social">
-  <? if (of_get_option('show_links')){ ;?>
-  	<ul>
-  		<?php if (of_get_option('facebook_url')){ ;?><li><a href="<?php echo of_get_option('facebook_url', 'no entry'); ?>" target="_blank" class="facebook">Facebook</a></li><?php } ?>
-  		<?php if (of_get_option('twitter_url')){ ;?><li><a href="<?php echo of_get_option('twitter_url', 'no entry'); ?>" target="_blank" class="twitter">Twitter</a></li><?php } ?>  			<?php if (of_get_option('youtube_url')){ ;?><li><a href="<?php echo of_get_option('youtube_url', 'no entry'); ?>" target="_blank" class="youtube">Youtube</a></li><?php } ?>
-  		<?php if (of_get_option('other_url')){ ;?><li><a href="<?php echo of_get_option('other_url', 'no entry'); ?>" target="_blank" class="other"><?php echo of_get_option('other_network_name', 'no entry'); ?></a></li><?php } ?>
-  	</ul>
-  <?php } ?>
-  <?php
-     if (has_nav_menu('social_navigation')) :
-       wp_nav_menu(array('theme_location' => 'social_navigation', 'menu_class' => 'nav'));
-     endif;
-  ?>
-  </div>
-      <p class="credits">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>  |  <a href="http://12southmusic.com/" target="_blank">built by 12SM</a></p>
+    <div class="col-md-6">
+      	<div class="credits">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>  |  <a href="http://12southmusic.com/" target="_blank">built by 12SM</a></div>
+    </div>
+    <div class="col-md-6">
+      <div class="row">
+      	<div class="col-md-8">
+      		<p>For in-depth web services, check out our sister company</p>
+      	</div>
+      	<div class="col-md-4">
+      		<img src="/assets/img/hifi-logo.png">
+      	</div>
+      </div>
     </div>
   </div>
 </footer>
