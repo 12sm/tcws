59c56e3b7447c4896d251181187d161d4f7a27d4
michael <michael@12southmusic.com>
2014-01-08 19:31:02 -0600

    Adding the .header height to the ScrollTo

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,9 +19,11 @@ var ExampleSite = {
 			    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 				    var target = $(this.hash);
 				    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				    var headerHeight = $(".banner").height();
+				    var scrollToPosition = $(target).offset().top - headerHeight;
 				    if (target.length) {
 					    $('html,body').animate({
-						    scrollTop: target.offset().top
+						    scrollTop: scrollToPosition
 						 }, 1000);
 						 return false;
 					}
