558fdf475c151dedeac9e47a92fe1a8d0bf6aee0
michael <michael@12southmusic.com>
2014-01-16 05:12:25 -0600

    test

--- templates/page-comments.php
+++ templates/page-comments.php
@@ -1,7 +1,1 @@
-<?php get_template_part('templates/head'); ?>
-<body>
-    <div class="content row">
-        <?php include roots_template_path(); ?>
-    </div><!-- /.content -->
-</body>
-</html>
+test
