c465eef83530905f11ba6cdfee44fd506409cadc
michael <michael@12southmusic.com>
2014-02-12 22:26:05 -0600

    Footer and Welder additions!

--- templates/footer.php
+++ templates/footer.php
@@ -5,16 +5,16 @@
 	</div>
 </div> 
   <div class="row">
-    <div class="col-md-8">
+    <div class="col-md-8 col-sm-6">
       	<div class="credits">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>  |  <a href="http://12southmusic.com/" target="_blank">built by 12SM</a></div>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-4 col-sm-6">
       <div class="row">
-      	<div class="col-md-8">
+      	<div class="col-md-8 col-sm-9">
       		<p>For in-depth web services, check out our sister company</p>
       	</div>
-      	<div class="col-md-4">
-      		<img src="/assets/img/hifi-logo.png">
+      	<div class="col-md-4 col-sm-3">
+      		<a href="http://hififusion.com" target="_blank"><img src="/assets/img/hifi-logo.png" class="footer-logo"></a>
       	</div>
       </div>
     </div>
